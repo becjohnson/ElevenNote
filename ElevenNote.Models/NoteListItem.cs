aebd43a79a3fdd6a3f4c7465b3962902e5fe4c3e
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-11-29 12:31:56 -0500

    Created NoteService

--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -5,22 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ElevenNote.Data
+namespace ElevenNote.Models
 {
-    public class Note
+    public class NoteListItem
     {
         [Key]
         public int NoteId { get; set; }
         [Required]
-        public Guid OwnerId { get; set; }
-        [Required]
-        [MaxLength(100, ErrorMessage ="There are too many characters in this field.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
         public string Title { get; set; }
         [Required]
-        public string Content { get; set; }
-        [Required]
-        [Display(Name = "Created")]
+        [Display(Name="Created")]
         public DateTimeOffset CreatedUtc { get; set; }
-        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
