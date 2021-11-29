6e93dcdd66e4ea10de0c9a63435f2ac845e841a1
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-11-29 15:06:29 -0500

    Delete API

--- ElevenNote.Models/NoteEdit.cs
+++ ElevenNote.Models/NoteEdit.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    class NoteEdit
+    public class NoteEdit
     {
+        [Key]
+        public int NoteId { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter as least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+        [MaxLength(8000)]
+        public string Content { get; set; }
     }
 }
--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -44,5 +44,27 @@ namespace ElevenNote.WebAPI.Controllers
             var note = noteService.GetNotebyId(id);
             return Ok(note);
         }
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var service = CreateNoteService();
+            if (!service.UpdateNotes(note))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+            if (!service.DeleteNote(id))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
     }
 }
